142dcb941d75234dc9beb44149476434fdb287e5
Bruno Vinicius <vruno.binicius@gmail.com>
2022-10-18 00:44:51 -0300

    feat: create image load animation

--- src/components/CardHeader/CardHeader.tsx
+++ src/components/CardHeader/CardHeader.tsx
@@ -1,11 +1,13 @@
+import { useCallback, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 /* import profilePhoto from '../../assets/profile.jpg'; */
-const profilePhoto = 'https://www.gravatar.com/avatar/12ad1eb4b5857717ac8801f9f62b9527?s=200';
+const profilePhoto =
+  "https://www.gravatar.com/avatar/12ad1eb4b5857717ac8801f9f62b9527?s=200";
 
 const CHWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;  
+  align-items: center;
   @media (max-width: 650px) {
     flex-direction: column;
     gap: 30px;
@@ -13,11 +15,13 @@ const CHWrapper = styled.div`
 `;
 
 const PhotoWrapper = styled.img`
+  opacity: 0;
   border-radius: 50%;
   aspect-radius: 1/1;
   width: 240px;
   flex: 1;
-`
+  transition: all ease 0.4s;
+`;
 
 const TitleWrapper = styled.div`
   flex: 2;
@@ -26,22 +30,37 @@ const TitleWrapper = styled.div`
   justify-content: center;
   align-items: center;
   > div {
-    > h1 {      
+    > h1 {
       font-size: 48px;
       font-weight: 900;
-      color: #1E91D6;      
+      color: #1e91d6;
     }
     > h2 {
       font-size: 36px;
       font-weight: 100;
     }
-  }  
-`
+  }
+`;
 
 export const CardHeader = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const onImageLoad = useCallback(() => {
+    setIsLoaded(true);
+  }, [setIsLoaded]);
+
   return (
     <CHWrapper>
-      <PhotoWrapper src={profilePhoto} alt="Bruno Vinicius smiling" />
+      <PhotoWrapper
+        onClick={() => {
+          setIsLoaded(!isLoaded);
+        }}
+        onLoad={onImageLoad}
+        style={{
+          ...(isLoaded && { opacity: "1" }),
+        }}
+        src={profilePhoto}
+        alt='Bruno Vinicius smiling'
+      />
       <TitleWrapper>
         <div>
           <h1>brunicius</h1>
